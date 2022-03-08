def1d8eb219cd67f100f9cc4b4bb24e5dd179610
Geo <geo@eggheads.org>
2022-03-08 16:17:49 -0500

    foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 sed -i -e '/edit your config file completely like you were told/d' \
        -e '/Please make sure you edit your config file completely/d' eggdrop.conf
 echo "utimer 5 die" >> eggdrop.conf
-exec ./eggdrop -nt -m eggdrop.conf
+echo "hello"
+exec ./eggdrop -t -m eggdrop.conf
 cat ~/eggdrop/pid.LamestBot
