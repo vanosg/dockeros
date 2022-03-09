94d954372a1a7388a8d60c574ce133d21fe288a0
Geo <geo@eggheads.org>
2022-03-08 20:01:09 -0500

    foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$(id -u)" = '0' ]; then
+  chown -R user /home/user/eggdrop .
+  exec su-exec user "$BASH_SOURCE" "$@"
+fi
+
 sed -i -e '/edit your config file completely like you were told/d' \
        -e '/Please make sure you edit your config file completely/d' eggdrop.conf
 echo "utimer 5 die" >> eggdrop.conf
