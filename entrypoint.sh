84cbc5907a1cbcb6ccbb8bdf5b9d349be4a8144b
Geo <geo@eggheads.org>
2022-03-14 19:46:56 -0400

    foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,10 @@ fi
 
 sed -i -e '/edit your config file completely like you were told/d' \
        -e '/Please make sure you edit your config file completely/d' eggdrop.conf
-echo "listen 0.0.0.0 3333 all" >> eggdrop.conf
+#echo "listen 0.0.0.0 3333 all" >> eggdrop.conf
 echo "set userfile eggdrop.user" >> eggdrop.conf
-echo "set pidfile \"/eggdata/pid.Lamestbot\"" >> eggdrop.conf
-#echo "utimer 10 die" >> eggdrop.conf
+#echo "set pidfile \"/eggdata/pid.Lamestbot\"" >> eggdrop.conf
+echo "utimer 4 die" >> eggdrop.conf
 echo "hello"
-exec ./eggdrop -m eggdrop.conf
+exec ./eggdrop -mt eggdrop.conf
 sleep 1
-ls -al /eggdata
-if [ -a /eggdata/pid.Lamestbot ]; then
-    echo "-=| TEST: PID: $(cat /eggdata/pid.Lamestbot)" > /eggdata/pidtest
-else
-    echo "-=| TEST: PID: Not running!" > /eggdata/pidtest
-fi
